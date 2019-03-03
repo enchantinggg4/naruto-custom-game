acebf815e618f887dc4bfa56b2ec06bd9aeb3e86
enchantinggg4 <enchnatinggg4@gmail.com>
2019-03-04 00:02:20 +0300

    fix sound gen

--- scripts/gen_soundevents.js
+++ scripts/gen_soundevents.js
@@ -74,7 +74,8 @@ function GenerateFiles(){
 
 	const enums = files.map(abilityName => {
 		const abilityFullPath = Path.join(__dirname, "../content/sounds/abilities", abilityName);
-		const data = GenerateSoundsEvents(abilityFullPath, Path.join(__dirname, "content/sounds"));
+		console.log(abilityFullPath);
+		const data = GenerateSoundsEvents(abilityFullPath, Path.join(__dirname, "../content/sounds"));
 		const soundevents = Path.join(__dirname, "../content/soundevents", data.fileName);
 		fs.writeFileSync(soundevents, data.code);
 		return data.enum
